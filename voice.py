b45cde87ead6b3f845ffb4a6c62b159ef5d55288
Mark Fickett <mark.fickett@gmail.com>
2014-07-31 23:47:54 -0400

    Fix parsing of multi-word voice names. Add voice.SayAllSamples.

--- voice.py
+++ voice.py
@@ -3,8 +3,16 @@ import random
 import subprocess
 
 
+class _VoiceSummary:
+  def __init__(self, summary_line):
+    voice_info, self.message = summary_line.split('#')
+    self.voice = ' '.join(voice_info.split()[:-1])
+
+
 __output = subprocess.check_output(['say', '-v', '?'])
-VOICES = [line.split()[0] for line in __output.split('\n') if line]
+VOICES = [
+    _VoiceSummary(summary_line).voice
+    for summary_line in __output.split('\n') if summary_line]
 VOICES_SET = frozenset(VOICES)
 RATE_WPM = '300'
 
@@ -19,8 +27,19 @@ def GetRandomVoice(exclude=None):
 DEFAULT_VOICE = GetRandomVoice()
 
 
-def Say(message, voice=DEFAULT_VOICE):
+def Say(message, voice=DEFAULT_VOICE, rate_wpm=RATE_WPM):
   if voice not in VOICES:
     raise ValueError(
         'voice %r not valid, should be one of %s' % (voice, VOICES))
-  subprocess.check_call(['say', '-v', voice, '-r', RATE_WPM, message])
+  subprocess.check_call(['say', '-v', voice, '-r', str(rate_wpm), message])
+
+
+def SayAllSamples():
+  """Speaks a line of sample text in each available voice."""
+  summary = subprocess.check_output(['say', '-v', '?'])
+  for summary_line in summary.split('\n'):
+    if not summary_line:
+      continue
+    info = _VoiceSummary(summary_line)
+    logging.info('%s: %s', info.voice, info.message)
+    Say(info.message, voice=info.voice, rate_wpm=500)
